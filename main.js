27935adca7988d29a25de7b6d22bb43905c05f96
Dtiger123 <88206528+Dtiger123@users.noreply.github.com>
2022-01-30 13:28:54 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(640,480);
+    canvas.position(110,250);
+    video=createCapture(VIDEO);
+    video.hide();
+    tintcolor="";
+}
+function draw(){
+    image(video,0,0,640,480);
+    tint(tintcolor);
+}
+function take_snapshot(){
+    save("myimage.png");
+}
+function filter1(){
+    tintcolor=document.getElementById("color_name").value;
+}
